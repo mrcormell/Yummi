3e60e5aea84c7e15600c8e0cb0dc748d1a966ee5
David Cormell <d.cormell@etoncollege.org.uk>
2023-12-11 11:21:31 +0000

    Initial Commit

---
+++ Yummi/YummiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YummiApp.swift
+//  Yummi
+//
+//  Created by Cormell, David - DPC on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct YummiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

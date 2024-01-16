addf795160e9d66b7e63138d9875b2e9317d9b83
David Cormell <d.cormell@etoncollege.org.uk>
2024-01-16 09:36:16 +0000

    TASK 2: First bullet point complete

--- Yummi/View/ContentView.swift
+++ Yummi/View/ContentView.swift
@@ -8,10 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    let myIngredient: Ingredient = Ingredient(name: "Minced Beef", quantity: 1, unit: "Kg", category: "Meat", expiry: "21st February 2024")
+    let myIngredients: [Ingredient] = [Ingredient(name: "Minced Beef", quantity: 1, unit: "Kg", category: "Meat", expiry: "21st February 2024"),Ingredient(name: "Minced Pork", quantity: 1, unit: "Kg", category: "Meat", expiry: "21st February 2024")]
+    @State private var currentIngredientIndex = 0
     
     var body: some View {
-        Text(myIngredient.display())
+        Form {
+            Section {
+                Text(myIngredients[currentIngredientIndex].display())
+                Button("Next") {
+                    if currentIngredientIndex >= myIngredients.count - 1 {
+                        currentIngredientIndex = 0
+                    } else {
+                        currentIngredientIndex += 1
+                    }
+                }
+            }
+        }
     }
 }
 

21f4e9fd65269f0f5218d97bf4d37e0fe7865960
David Cormell <d.cormell@etoncollege.org.uk>
2024-02-21 16:06:35 +0000

    STAGE 5 - Extra Hard extension complete (stars to indicate rating)

---
+++ Yummi/View/StarRatingView.swift
@@ -0,0 +1,18 @@
+//
+//  StarRatingView.swift
+//  Yummi
+//
+//  Created by Cormell, David - DPC on 21/02/2024.
+//
+
+import SwiftUI
+
+struct StarRatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StarRatingView()
+}

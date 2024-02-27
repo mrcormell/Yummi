f9be8bb496f45b724d19479e140232a97474d37f
David Cormell <d.cormell@etoncollege.org.uk>
2024-02-27 15:56:43 +0000

    Final view model extraction of recipes view model

--- Yummi/View/RecipesView.swift
+++ Yummi/View/RecipesView.swift
@@ -8,24 +8,12 @@
 import SwiftUI
 
 struct RecipesView: View {
-    var recipes: [Recipe] = Recipe.examples
-    
-    var sortedByRatingRecipes: [Recipe] {
-        if searchText.isEmpty {
-            return recipes.sorted(by: { $0.rating > $1.rating })
-        } else {
-            return recipes.filter { $0.name.contains(searchText) }
-                .sorted(by: { $0.rating > $1.rating })
-        }
-        
-    }
-    
-    @State private var searchText = ""
+    @State private var recipesViewModel = RecipesViewModel()
     
     var body: some View {
         NavigationStack {
             List {
-                ForEach(sortedByRatingRecipes, id: \.name) { recipe in
+                ForEach(recipesViewModel.sortedByRatingRecipes, id: \.name) { recipe in
                     NavigationLink(destination: {
                         RecipeDetailView(recipe: recipe)
                     }, label: {
@@ -35,7 +23,7 @@ struct RecipesView: View {
                 }
             }
             .navigationTitle("Recipes")
-            .searchable(text: $searchText)
+            .searchable(text: $recipesViewModel.searchText)
         }
     }
 }
---
+++ Yummi/ViewModel/RecipesViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  RecipesViewModel.swift
+//  Yummi
+//
+//  Created by Cormell, David - DPC on 27/02/2024.
+//
+
+import Foundation
+
+@Observable
+class RecipesViewModel {
+    var recipes: [Recipe] = Recipe.examples
+    
+    var sortedByRatingRecipes: [Recipe] {
+        if searchText.isEmpty {
+            return recipes.sorted(by: { $0.rating > $1.rating })
+        } else {
+            return recipes.filter { $0.name.contains(searchText) }
+                .sorted(by: { $0.rating > $1.rating })
+        }
+        
+    }
+    
+    var searchText = ""
+}
